3c86e316c08b5494d3a3db2c6fb6df976da405f8
Trevor Huff <trevh5@gmail.com>
2017-07-15 21:46:33 -0500

    new file:   WebScraper2.py

---
+++ WebScraper2.py
@@ -0,0 +1,28 @@
+#!/bin/python
+
+import os
+
+inputDir = '\\Users\\Trevor\\Desktop\\webparsed\\'
+outputDir = '\\Users\\Trevor\\Desktop\\attorneyWebsites\\'
+
+wget = r'\Users\Trevor\Desktop\wget-1.19.1-win64\wget'
+commands = ' --user-agent=Mozilla -e robots=off -q '
+
+for filename in os.listdir(inputDir):
+	print("--------------------------------------------------------")
+	print(filename)
+	os.mkdir(outputDir+ filename.rpartition('.')[0])
+	count = 1
+	lines = []
+	with open(inputDir + filename) as file:
+		for line in file:
+			print("Accessing Website: " + line)
+			url = line
+			print(' -O \"\\Users\\Trevor\\Desktop\\attorneyWebsites\\' + filename.rpartition('.')[0] + str(count) + ".txt\"")
+			output = ' -O \"' + outputDir + filename.rpartition('.')[0] + "\\" + str(count) + ".txt\" "
+			print(wget + commands + output + url)
+			os.system(wget + commands + output + url)
+			count=count+1
+	print("--------------------------------------------------------")
+
+print("DONE!")
