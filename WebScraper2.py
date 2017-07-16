8f257e63e9ec90dcfddc8dd9879eae77a157d69b
Trevor Huff <trevh5@gmail.com>
2017-07-15 21:48:49 -0500

    Update WebScraper2.py

--- WebScraper2.py
+++ WebScraper2.py
@@ -1,9 +1,9 @@
 #!/bin/python
-
+# This web scraper was used to download a list of websites that were retrieved from a search engine
 import os
 
 inputDir = '\\Users\\Trevor\\Desktop\\webparsed\\'
-outputDir = '\\Users\\Trevor\\Desktop\\attorneyWebsites\\'
+outputDir = '\\Users\\Trevor\\Desktop\\--\\'
 
 wget = r'\Users\Trevor\Desktop\wget-1.19.1-win64\wget'
 commands = ' --user-agent=Mozilla -e robots=off -q '
@@ -18,7 +18,7 @@ for filename in os.listdir(inputDir):
 		for line in file:
 			print("Accessing Website: " + line)
 			url = line
-			print(' -O \"\\Users\\Trevor\\Desktop\\attorneyWebsites\\' + filename.rpartition('.')[0] + str(count) + ".txt\"")
+			print(' -O \"\\Users\\Trevor\\Desktop\\--\\' + filename.rpartition('.')[0] + str(count) + ".txt\"")
 			output = ' -O \"' + outputDir + filename.rpartition('.')[0] + "\\" + str(count) + ".txt\" "
 			print(wget + commands + output + url)
 			os.system(wget + commands + output + url)
